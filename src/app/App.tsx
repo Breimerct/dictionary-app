505b2d2ee008af07ba3545f023f227c1bb93dc62
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-22 06:55:58 -0500

    implement search input

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
 import Header from '@/components/header/Header';
 import './App.scss';
+import SearchInput from '@/components/search-input/SearchInput';
 
 function App() {
+    const handleSubmit = (searchText: string) => {
+        console.log(searchText);
+    };
+
     return (
         <main className="min-h-screen md:w-[70%] mx-auto">
             <Header />
             <h1 className="text-6xl font-bold">Dictionary app</h1>
+            <SearchInput onSubmit={handleSubmit} />
         </main>
     );
 }
