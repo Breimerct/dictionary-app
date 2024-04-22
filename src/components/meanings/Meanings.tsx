dec6e80c45ee2acb98c95fda9cc1b31d8b118824
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-22 10:17:35 -0500

    fixed spaces

--- src/components/meanings/Meanings.tsx
+++ src/components/meanings/Meanings.tsx
@@ -8,7 +8,7 @@ interface MeaningsProps {
 const Meanings: FC<MeaningsProps> = ({ meanings }) => {
     return meanings.map(
         ({ partOfSpeech, definitions, antonyms, synonyms }, index) => (
-            <article key={index}>
+            <article key={index} className="mt-4">
                 <h1 className="text-2xl font-bold capitalize">
                     {partOfSpeech}
                 </h1>
@@ -31,7 +31,7 @@ const Meanings: FC<MeaningsProps> = ({ meanings }) => {
                 )}
 
                 {antonyms.length > 0 && (
-                    <article className="flex gap-1">
+                    <article className="flex gap-1 mt-6">
                         <p className="font-bold m-0">antonyms: </p>
                         <span className="text-purple-600 dark:text-purple-400">
                             {antonyms.join(', ')}
