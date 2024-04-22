27cca98d6192233b94ea0d9040a815d2c9920051
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-22 05:04:17 -0500

    create project

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { ViteAliases } from 'vite-aliases';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        ViteAliases({
+            dir: 'src', // Directorio base donde se buscarán las carpetas
+            prefix: '@', // Prefijo para los alias de rutas
+            deep: true, // Permite buscar en subdirectorios
+            depth: 1, // Nivel de profundidad para la búsqueda de subdirectorios (1 para el primer nivel)
+            createGlobalAlias: true, // Crea un alias global para el directorio del proyecto
+        }),
+    ],
+});
