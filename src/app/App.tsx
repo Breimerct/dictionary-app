91d0035bb7f3bbbfdd6c12b95f34420c1e86dc9e
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-22 09:58:17 -0500

    implemente new components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import Header from '@/components/header/Header';
-import './App.scss';
 import SearchInput from '@/components/search-input/SearchInput';
+import { getDefinition } from '@/services/dictionary.service';
+import { DefinitionResult } from '@/types';
+import './App.scss';
+import DefinitionList from '@/components/definition-list/DefinitionList';
 
 function App() {
-    const handleSubmit = (searchText: string) => {
-        console.log(searchText);
+    const [definitions, setDefinitions] = useState<DefinitionResult[]>();
+    const handleSubmit = async (searchText: string) => {
+        try {
+            const dataResult = await getDefinition(searchText);
+
+            setDefinitions(dataResult);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
-        <main className="min-h-screen md:w-[70%] mx-auto">
+        <main className="min-h-screen md:w-[70%] lg:w-[50%] mb-10 mx-6 md:mx-auto">
             <Header />
-            <h1 className="text-6xl font-bold">Dictionary app</h1>
             <SearchInput onSubmit={handleSubmit} />
+
+            {definitions && <DefinitionList definitions={definitions} />}
         </main>
     );
 }
