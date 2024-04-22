a3c517e5b4e06fe76f039c5d241c5fc375dc3230
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-22 05:58:46 -0500

    add type return function

--- src/services/dark.service.ts
+++ src/services/dark.service.ts
@@ -1,4 +1,4 @@
-export const getLocalStoreDark = () => {
+export const getLocalStoreDark = (): boolean => {
     const isActivaDarkTheme = localStorage.getItem('dark');
 
     if (isActivaDarkTheme == undefined) return false;
